a229a89c9cd4e0d5afe6312f3103776d2255ca6a
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-08-21 16:59:50 -0400

    cleanup

--- app.js
+++ app.js
@@ -4,16 +4,11 @@ const users = require('./users.js');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
-// Amy also had this one:
-//const parseurl = require('parseurl');
-
 
 // setup view engine
 app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
-//app.use('/', adminRouter);
-// app.use('/admin', adminRouter);
 
 // middleware
 app.use(bodyParser.json());
@@ -25,25 +20,17 @@ app.use(session({
   saveUninitialized: true
 }));
 
-// function match(user){
-//   return user.username === username;
-// }
-
 function authenticate(req,username,password){
   if (user = users.users.find(function(x){
     return x.username === username;
   })) {
   if (user.password === password) {
     console.log ("yay! password is right!");
-    // console.log(req.session);
     function randomNumber(min, max) {
       let random =  Math.random() * (max - min) + min;
       req.session.cookie.id = random;
-      // console.log(req.session.cookie.id);
-      // console.log(req.session);
     }
     randomNumber(1, 101);
-
   }
   else {
     console.log ("nope, password is wrong.")
@@ -51,38 +38,16 @@ function authenticate(req,username,password){
   else {
     console.log("sorry, username not found.")
   }
-  // console.log("Is this the right user?: "+user.username);
 };
 
-
-
-
-// for (var i = 0; i < users.users.length; i++) {
-//   if (users.users[i].username === username && users.users[i].password === password) {
-//     console.log("AUTHENTICATED!!");
-//   }
-//   else {
-//     console.log("None of them matched!");
-//   }
-// }
-// }
-// GET/POST
-
 app.get('/',function(req,res){
-  //res.send("Hello world!");
   res.render('index')
 });
 
 app.post('/', function(req, res){
-  //console.log(req.body);
   var username = req.body.username;
   var password = req.body.password;
-  //console.log(username);
-  //console.log(password);
   authenticate(req,username,password);
-  //console.log(req.session);
-  // console.log(users);
-  // console.log(users.users[0]);
   if (req.session.cookie.id) {
     res.render('yay', {username})
   }
@@ -91,29 +56,6 @@ app.post('/', function(req, res){
   }
 });
 
-
 app.listen(3000, function(){
   console.log('Example app listening on port 3000!')
 });
-
-// call login function in admin.js in POST
-
-// app.get('/', function(req, res){
-//   // Set 'action' to '/'
-//   var html = '<form action="/" method="post">' +
-//              '<h1>User Name</h1>' +
-//              '<p>Enter your email</p>' +
-//              '<input type="text" name="email" placeholder="email address" />' +
-//              '<button type="submit">Submit</button>' +
-//          '</form>';
-//   res.send(html);
-// });
-//
-// // Receives data from form (action='/')
-// // 'req.body' now contains form data.
-// app.post('/', function(req, res){
-//   console.log(req.body);
-//   var email = req.body.email;
-//   var html = '<p>Your user name is: </p>' + email;
-//   res.send(html);
-// });
